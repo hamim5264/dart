607129e16cf0a429ca7d45d79428e3fa54790ee1
MD. ABDUL HAMIM <hamim15-5264@diu.edu.bd>
2023-08-14 21:26:38 +0600

    updated

---
+++ bin/code_mama/module3_1.dart
@@ -0,0 +1,15 @@
+import 'dart:io';
+void main (){
+  String? str = stdin.readLineSync();
+  for(var i =0; i<str!.length; i++){
+    if (str.contains('a')||str.contains('e')||str.contains('i')||str.contains('o')||
+    str.contains('u')||str.contains('A')||str.contains('E')||str.contains('I')||
+    str.contains('O')||str.contains('U')){
+      print("The string contains a vowel.");
+      break;
+    }else {
+      print("The string does not contain any vowel.");
+      break;
+    }
+  }
+}
