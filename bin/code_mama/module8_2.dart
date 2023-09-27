a50130dc87133b47609aeaf7e2774d10b418059d
MD. ABDUL HAMIM <hamim15-5264@diu.edu.bd>
2023-09-27 20:15:23 +0600

    updated

---
+++ bin/code_mama/module8_2.dart
@@ -0,0 +1,11 @@
+import 'dart:io';
+void main() {
+  int count = 0;
+  for (int i = 0; i <10; i++) {
+    String? input = stdin.readLineSync();
+    if (input!.contains('T')) {
+      count++;
+    }
+  }
+  print(count);
+}
