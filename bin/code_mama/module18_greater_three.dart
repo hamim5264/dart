7d7234b5029094fc27ca64506ae08fd2a26e29ab
MD. ABDUL HAMIM <hamim15-5264@diu.edu.bd>
2024-01-05 20:32:38 +0600

    updated

---
+++ bin/code_mama/module18_greater_three.dart
@@ -0,0 +1,15 @@
+import 'dart:io';
+void main (){
+  String? str = stdin.readLineSync();
+  var space = str!.split(" ");
+  int x = int.parse(space[0]);
+  int y = int.parse(space[1]);
+  int z = int.parse(space[2]);
+  if(x>y && x>z){
+    print(x);
+  }else if(y>x && y>z){
+    print(y);
+  }else{
+    print(z);
+  }
+}
